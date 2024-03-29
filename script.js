1c6b801c6b2c47e0121c6161b0a2221cedef0d18
Richard Song <rsong1993@gmail.com>
2024-03-29 10:39:12 -0700

    Instantiated OpenAI model

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
+const { OpenAI } = require('langchain/llms/openai');
 require('dotenv').config();
-console.log(process.env.OPENAI_API_KEY)
+
+const model = new OpenAI({ 
+    openAIApiKey: process.env.OPENAI_API_KEY, 
+    temperature: 0,
+    model: 'gpt-3.5-turbo'
+  });
+  
+//   console.log({ model });
